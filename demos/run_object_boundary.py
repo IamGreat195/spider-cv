cff248ea81e7c85042695eac0775edf4d1e13220
Lelouch <67532246+IamGreat195@users.noreply.github.com>
2026-01-16 21:51:14 +0530

    Fixed src folder

--- demos/run_object_boundary.py
+++ demos/run_object_boundary.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# add project root to PYTHONPATH
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from src.object_boundary.boundary import main
 
 if __name__ == "__main__":
--- src/object_boundary/boundary.py
+++ src/object_boundary/boundary.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import cv2 as cv
-import numpy as np
 from collections import deque
 
 def main():
